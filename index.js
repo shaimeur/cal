3c2ec01395d44dbfc8af17663e8d99cf6bb6a6b2
shaimeur <haimeursaad@gmail.com>
2023-08-21 21:36:48 +0100

    finalazing the style of the calculator

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('cal')
+const display = document.querySelector('.display');
+const clear = document.querySelector('.clear');
+const del = document.querySelector('.del');
+const division = document.querySelector('.division');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const multiplication = document.querySelector('.multiplication');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const substraction = document.querySelector('.substraction');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const addition = document.querySelector('.addition');
+const zeroo = document.querySelector('.zero');
+const equal = document.querySelector('.equal');
+
+
+
+
+seven.addEventListener("click",function (event){
+   console.log(event.target.value)
+})
