0948ec13a479aece12d97e75c3602a21edf40d38
shaimeur <haimeursaad@gmail.com>
2023-08-27 01:22:57 +0100

    creating the operation for the calculator I found some difficulty but after I put the firstValue and the operationValue in the global scope its strated to make sens I have implimented the result in the equal section I still need some inhencments to improve my calculator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // the display screen
    const display = document.querySelector('.display');
-
+   let first = '';
+   let selectedOp = '';
    // const display1 = document.getElementById("screen");
    // const display = document.getElementsByClassName("display");
    // const display2 = document.getElementsByTagName('div');
@@ -30,81 +31,142 @@ const seven = document.querySelector('.seven');
 const eight = document.querySelector('.eight');
 const nine = document.querySelector('.nine');
 
-function catchDisplayValue(){
-   console.log(display.innerHTML)
+
+function calculate(x,op,y){
+  if(op === '+'){
+   return Number(x )+ Number(y) ;
+  }else if (op === '−') {
+      return x - y
+  } else if (op === '×') {
+      return x * y ;
+  } else if(op === '÷'){
+      if(y === 0){
+         return console.log('not possible')
+      }
+      return x / y
+  }
 }
 
-
-
 zeroo.addEventListener("click",function (event){
-   let resultOfZero = event.target.innerHTML
+   let resultOfZero = event.target.innerText
    // console.log(resultOfZero)
    // console.log(display)
    // console.log(display.append(resultOfZero))
    return display.append(resultOfZero)
 })
 one.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfOne = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfOne = event.target.innerText
    return display.append(resultOfOne)
 })
 two.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfTwo = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfTwo = event.target.innerText
    return display.append(resultOfTwo)
 })
 three.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfThree = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfThree = event.target.innerText
    return display.append(resultOfThree)
 })
 four.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfFour = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfFour = event.target.innerText
    return display.append(resultOfFour)
 })
 five.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfFive = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfFive = event.target.textContent
    return display.append(resultOfFive)
 })
 six.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfSix = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfSix = event.target.innerText
    return display.append(resultOfSix)
 })
 seven.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfSeven = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfSeven = event.target.innerText
    return display.append(resultOfSeven)
 })
 eight.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfEight = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfEight = event.target.innerText
    return display.append(resultOfEight)
 })
 nine.addEventListener("click",function (event){
-   // console.log(event.target.innerHTML)
-   let resultOfNine = event.target.innerHTML
+   // console.log(event.target.innerText)
+   let resultOfNine = event.target.innerText
    return display.append(resultOfNine)
 })
 
 clear.addEventListener("click",function(){
-      catchDisplayValue()
      return  display.innerText = ''
 })
 
 del.addEventListener("click",function(){
-   console.log(display.innerHTML)
-   // console.log(typeof display.innerHTML)
-   let putedVal =  display.innerHTML.split('')
-   console.log('input',putedVal)
-   // console.log(putedVal.at(-1))
-   putedVal.pop()
+
+   let putedVal =  display.innerText.split('')
+       putedVal.pop()
    let r =  putedVal.join('')
-   console.log(typeof r)
-   console.log("after the traitement", r)
-     display.innerHTML = r
-   console.log('result',r)
+     return display.innerText = r
+})
+
+addition.addEventListener('click',function(event){
+
+   first = display.innerText;
+
+   display.innerText = '';
+
+   selectedOp = event.target.innerText;
+
+   display.innerText = ''
 
 })
+multiplication.addEventListener('click',function(event){
+
+   first = display.innerText;
+
+   display.innerText = '';
+
+   selectedOp = event.target.innerText;
+
+   display.innerText = ''
+
+})
+division.addEventListener('click',function(event){
+
+   first = display.innerText;
+
+   display.innerText = '';
+
+   selectedOp = event.target.innerText;
+
+   display.innerText = ''
+
+})
+substraction.addEventListener('click',function(event){
+
+   first = display.innerText;
+
+   display.innerText = '';
+
+   selectedOp = event.target.innerText;
+
+   display.innerText = ''
+
+})
+
+equal.addEventListener('click', function(){
+
+   let  second = display.innerText
+   let result = ''
+   display.innerText = ''
+   result = calculate(first,selectedOp,second)
+   console.log(result)
+   return display.append(result)
+})
+
+
+
+
